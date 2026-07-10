13b07a2649ccbead794fa181bb948e908d01c2c4
Vaishnavi Parmar <vaishnaviparmar925@gmail.com>
2026-07-10 15:45:12 +0530

    Solved LeetCode 724 - Find Pivot Index

---
+++ Easy/724_Find_Pivot_Index.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        total = sum(nums)
+        left = 0
+
+        for i in range(len(nums)):
+
+            right = total - left - nums[i]
+
+            if left == right:
+                return i
+
+            left += nums[i]
+
+        return -1
+    
+# Time Complexity: O(n)
+# Space Complexity: O(1)
