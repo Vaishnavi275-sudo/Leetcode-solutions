01e7a61d1d49864fe1940abe86d05cb0e5852235
Vaishnavi Parmar <vaishnaviparmar925@gmail.com>
2026-07-16 13:12:14 +0530

    Solved 448

---
+++ Easy/448_Find_All_Numbers_Disappeared_in_an_Array.py
@@ -0,0 +1,12 @@
+def findDisappearedNumbers(self, nums):
+        
+    s = set(nums)
+
+    ans = []
+
+    for i in range(1, len(nums)+1):
+
+        if i not in s:
+            ans.append(i)
+
+    return ans   
